966dff598e674ba76cdd0129fbd34a349bebc173
Dave Turissini <davidairborne@gmail.com>
2014-02-08 22:06:01 -0800

    Removing mobile checks

--- src/server/backbone.js
+++ src/server/backbone.js
@@ -3,7 +3,6 @@ var _ = require('underscore');
 var jquery = require('jquery');
 var Q = require('q');
 Backbone.$ = jquery;
-var isMobile = require('./../util/isMobile');
 
 var routes = [];
 var layoutsDirectory = '/layouts';
@@ -131,10 +130,6 @@ var backboneServer = {
 		var server = this;
 		var htmlEl = jquery('html');
 		var contentEl = jquery('.content');
-
-		if (isMobile) {
-			return;
-		}
 		
 		routes.reverse().forEach(function (routeData) {
 
